3be3cd67620172aea2e4302d97c5eb209b212fcd
Brent Bauman <brentbauman86@gmail.com>
2018-04-24 20:20:46 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,10 @@ def key_for_min_value(name_hash)
 lowest_key = nil
 lowest_value = nil
 name_hash.each do |key, value|
-if lowest_value == nil && value.start_with?("a")
-  lowest = key
+if lowest_value == nil || value < lowest_value
+  lowest_value = value
+  lowest_key = key
   end
 end
-lowest
+lowest_key
 end
